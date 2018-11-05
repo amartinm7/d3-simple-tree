d7e4a5983ff59c7c41b5e8412f827f71ee4d423f
antoniomm <antonio.martinmoreno@zooplus.com>
2018-11-05 16:34:22 +0100

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# component-tree
+# component-tree: Simple d3.js
+![simple-tree](./_media/D3JS.png)
+
+Vue project to display simple tree diagrams.
+Given a data set the d3 can display a tree diagram using the SVG technology.
 
 ## Project setup
 ```
@@ -27,3 +31,14 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### References
+Simple d3.js Tree Diagram with styling, see http://bl.ocks.org/d3noob/8324872. 
+Take into a count that the d3 lib version is the v3.5.17. The lib is really different for the v5.
+
+Result:
+
+![simple-tree](./_media/tree-01.png)
+
+
+More documentation in https://d3js.org/
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,147 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div>
+
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  const d3 = require('d3')
+
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    },
+    data() {
+      return {
+        treeData: [
+          {
+            "name": "Top Level",
+            "parent": "null",
+            "value": 10,
+            "type": "black",
+            "level": "red",
+            "children": [
+              {
+                "name": "Level 2: A",
+                "parent": "Top Level",
+                "value": 15,
+                "type": "grey",
+                "level": "red",
+                "children": [
+                  {
+                    "name": "Son of A",
+                    "parent": "Level 2: A",
+                    "value": 5,
+                    "type": "steelblue",
+                    "level": "orange"
+                  },
+                  {
+                    "name": "Daughter of A",
+                    "parent": "Level 2: A",
+                    "value": 8,
+                    "type": "steelblue",
+                    "level": "red"
+                  }
+                ]
+              },
+              {
+                "name": "Level 2: B",
+                "parent": "Top Level",
+                "value": 10,
+                "type": "grey",
+                "level": "green"
+              }
+            ]
+          }
+        ],
+        margin: {top: 20, right: 120, bottom: 20, left: 120},
+      }
+    },
+    computed: {
+      width: function () {
+        return 960 - this.margin.right - this.margin.left
+      },
+      height: function () {
+        return 500 - this.margin.top - this.margin.bottom
+      },
+      root: function () {
+        return this.treeData[0]
+      }
+
+    },
+    watch: {},
+    methods: {
+      update: function (tree, root, diagonal, svg) {
+        let i = 0
+        // Compute the new tree layout.
+        const nodes = tree.nodes(root).reverse()
+        const links = tree.links(nodes)
+
+        // Normalize for fixed-depth.
+        nodes.forEach(function(d) { d.y = d.depth * 180; })
+
+        // Declare the nodes…
+        const node = svg.selectAll("g.node").data(nodes, function(d) { return d.id || (d.id = ++i) })
+
+        // Enter the nodes.
+        const nodeEnter = node.enter().append("g")
+          .attr("class", "node")
+          .attr("transform", function(d) {
+            return "translate(" + d.y + "," + d.x + ")" })
+
+        nodeEnter.append("circle")
+          .attr("r", function(d) { return d.value })
+          .style("stroke", function(d) { return d.type })
+          .style("fill", function(d) { return d.level })
+
+        nodeEnter.append("text")
+          .attr("x", function(d) {
+            return d.children || d._children ?
+              (d.value + 4) * -1 : d.value + 4 })
+          .attr("dy", ".35em")
+          .attr("text-anchor", function(d) {
+            return d.children || d._children ? "end" : "start" })
+          .text(function(d) { return d.name })
+          .style("fill-opacity", 1)
+
+        // Declare the links…
+        const link = svg.selectAll("path.link")
+          .data(links, function(d) { return d.target.id })
+
+        // Enter the links.
+        link.enter().insert("path", "g")
+          .attr("class", "link")
+          .style("stroke", function(d) { return d.target.level })
+          .attr("d", diagonal)
+      },
+    },
+    mounted: function () {
+      const tree = d3.layout.tree().size([this.height, this.width])
+      const diagonal = d3.svg.diagonal().projection(function(d) { return [d.y, d.x] })
+      const svg = d3.select('body').append("svg:svg")
+        .attr("width", this.width + this.margin.right + this.margin.left)
+        .attr("height", this.height + this.margin.top + this.margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")")
+      this.update(tree, this.root, diagonal, svg)
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+    .node circle {
+        fill: #fff;
+        stroke: steelblue;
+        stroke-width: 3px;
+    }
+
+    .node text { font: 12px sans-serif; }
+
+    .link {
+        fill: none;
+        stroke: #ccc;
+        stroke-width: 2px;
+    }
 </style>
